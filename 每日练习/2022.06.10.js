8d9896a86c73d6be99bd7ecc2e609ccb405fcda2
JacobMa1996 <934273746@qq.com>
2022-06-11 16:07:00 +0800

    feat: some

---
+++ 每日练习/2022.06.10.js
@@ -0,0 +1,30 @@
+// 实现一个bind
+Function.prototype.bind = function (context) {
+  if (typeof this != 'function') {
+    throw new TypeError('error')
+  }
+
+  let self = this
+  const args = [...arguments].slice(1)
+  const F = function () {
+    return self.apply(
+      this instanceof self ? this : context,
+      args.concat(...arguments)
+    )
+  }
+  F.prototype = self.prototype
+
+  return F
+}
+
+// 使用
+function foo(name) {
+  this.name = name
+}
+var obj = {}
+var bar = foo.bind(obj)
+bar('Jack')
+console.log(obj.name) // Jack
+var alice = new bar('Alice')
+console.log(obj.name) // Jack
+console.log(alice.name) // Alice
