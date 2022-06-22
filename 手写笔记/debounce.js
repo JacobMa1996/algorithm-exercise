2396b281ea3670ca1d85efca7c7c07958779cda7
JacobMa1996 <934273746@qq.com>
2022-06-23 02:07:15 +0800

    feat: some

--- 手写笔记/debounce.js
+++ 手写笔记/debounce.js
@@ -1,47 +1,47 @@
 function debounce(fn, delay) {
-    let timer,
-        context = this;
+  let timer,
+    context = this
 
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(context, args);
-        }, delay);
-    };
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(context, args)
+    }, delay)
+  }
 }
 
 function debounce2(fn, delay, immediate) {
-    let timer,
-        context = this;
+  let timer,
+    context = this
 
-    return (...args) => {
-        if (timer) clearTimeout(timer);
+  return (...args) => {
+    if (timer) clearTimeout(timer)
 
-        if (immediate) {
-            let callNow = !timer;
-            timer = setTimeout(() => {
-                timer = null;
-            }, delay);
+    if (immediate) {
+      let callNow = !timer
+      timer = setTimeout(() => {
+        timer = null
+      }, delay)
 
-            callNow && fn.apply(context, args);
-        } else {
-            timer = setTimeout(() => {
-                fn.apply(context, args);
-            }, delay);
-        }
-    };
+      callNow && fn.apply(context, args)
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+      }, delay)
+    }
+  }
 }
 
 function throttled(fn, delay) {
-    let timer,
-        context = this;
+  let timer,
+    context = this
 
-    return (...args) => {
-        if (!timer) {
-            timer = setTimeout(() => {
-                fn.apply(context, args);
-                timer = null;
-            });
-        }
-    };
+  return (...args) => {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+        timer = null
+      }, delay)
+    }
+  }
 }
--- 手写笔记/sort.js
+++ 手写笔记/sort.js
@@ -1,11 +1,11 @@
 // 生成从l到r的数量为n的随机数组
 function randomArr(n, l, r) {
-    let arr = [];
-    for (let i = 0; i < n; i++) {
-        let _random = Math.round(l + Math.random() * (r - l));
-        arr.push(_random);
-    }
-    return arr;
+  let arr = []
+  for (let i = 0; i < n; i++) {
+    let _random = Math.round(l + Math.random() * (r - l))
+    arr.push(_random)
+  }
+  return arr
 }
 // function buddleSort(arr) {
 //     let len = arr.length;
@@ -21,34 +21,83 @@ function randomArr(n, l, r) {
 
 // 冒泡排序
 function sort1(arr) {
-    const array = [...arr];
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
+  const array = [...arr]
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
     }
-    return array
+  }
+  return array
 }
 
 // 冒泡排序，减少第二次循环次数
 function sort2(arr) {
-    const array = [...arr];
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
+  const array = [...arr]
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
     }
-    return array
+  }
+  return array
 }
+
+// 快速排序
+function sort3(array) {
+  // const array = [...arr]
+  if (array.length < 2) return array
+  let left = [],
+    right = [],
+    midIndex = Math.floor(array.length / 2)
+  let mid = array.splice(midIndex, 1)[0]
+  // console.log(midIndex)
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mid) {
+      left.push(array[i])
+    } else {
+      right.push(array[i])
+    }
+  }
+
+  return sort3(left).concat(mid, sort3(right))
+}
+
+// 快速排序
+function sort4(array) {
+  if (array.length < 2) return array
+  const quickSort = (nums, left, right) => {
+    if (left >= right) return
+    let i = left,
+      j = right - 1
+    while (i <= j) {
+      if (nums[i] > nums[right]) {
+        ;[nums[i], nums[j]] = [nums[j], nums[i]]
+        j--
+      } else {
+        i++
+      }
+    }
+    j++
+    ;[nums[j], nums[right]] = [nums[right], nums[j]]
+
+    quickSort(nums, left, j - 1)
+    quickSort(nums, j + 1, right)
+  }
+
+  quickSort(array, 0, array.length - 1)
+  return array
+}
+
 const arr = randomArr(10, 5, 100)
 console.log(arr)
 console.log(sort1(arr))
 console.log(sort2(arr))
+console.log(sort3(arr))
+console.log(sort4(arr))
