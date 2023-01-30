c48b86a4c8a6dbaceceb8d4322dfcb67d1f78e05
JacobMa1996 <934273746@qq.com>
2023-01-30 15:59:04 +0800

    feat: update

--- 每日练习/2023.01.30.js
+++ 每日练习/2023.01.30.js
@@ -41,3 +41,18 @@ function create(prototype) {
 let b = new Child()
 
 b.funcA()
+
+// 箭头函数与普通函数的区别
+
+// 1.箭头函数内的this指向外层函数，如果外层没有函数，指向window，如果是class内的方法，指向new出来的实例
+// 2.箭头函数没有prototype，不可new，也不可用apply或call修改this执行
+// 3.箭头函数的this是在声明时定义的，而普通函数的this是在执行时定义的
+// 4.箭头函数的arguments也继承自外层函数，如果是在全局，会报错
+
+// 实现一个new
+function myNew(C, ...args) {
+    let obj = {}
+    obj.__proto__ = C.prototype
+    let result = C.apply(obj, ...args)
+    return result instanceof Object ? result : obj
+}
